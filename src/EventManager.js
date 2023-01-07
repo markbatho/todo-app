dc952d4622f625f286e459f2abbad3af34f20aa4
Mark Batho <mark747488@gmail.com>
2023-01-07 18:25:32 +0100

    Create EventManager (observer-like pattern)

---
+++ src/EventManager.js
@@ -0,0 +1,25 @@
+class EventManager {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, handler) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(handler);
+  }
+
+  off(event, handler) {
+    const index = this.events[event].indexOf(handler);
+    if (index > -1) {
+      this.events[event].splice(index, 1);
+    }
+  }
+
+  emit(event, data) {
+    this.events[event].forEach((handler) => handler(data));
+  }
+}
+
+export default EventManager;
